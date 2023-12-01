3fb56a8c92c3af819bef091bca592c0e2e3de952
oumaima <daoudioumaima300@gmail.com>
2023-12-01 13:02:35 +0100

    Add project files.

---
+++ login_page_v2/Form1.cs
@@ -0,0 +1,64 @@
+
+using System.Data.SqlClient;
+
+
+namespace login_page_v2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = textBox1.Text;
+            string password = textBox2.Text;
+
+            if (IsValidUser(username, password))
+            {
+                MessageBox.Show("Authentification réussie ! Vous pouvez accéder à l'application.", "Succès", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Nom d'utilisateur ou mot de passe incorrect. Accès refusé.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+
+
+        private bool IsValidUser(string username, string password)
+        {
+            string connectionString = "Data Source=OUMAIMA_HP\\SQLEXPRESS;Initial Catalog=testdatabase;Integrated Security=True";
+
+            try  
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = "SELECT COUNT(*) FROM [user_table] WHERE username = @Username AND password = @Password";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Username", username);
+                        command.Parameters.AddWithValue("@Password", password);
+
+                        int count = (int)command.ExecuteScalar();
+
+                        return count > 0;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+    }
+
+
+}
